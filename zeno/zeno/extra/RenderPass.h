bfc57f6c2639deec633ef34b3109fe1dc7963eaf
zydiii <zydiiii@outlook.com>
2022-07-09 05:05:58 +0800

    [Add Class] ResourceNode & RenderPass

---
+++ zeno/zeno/extra/RenderPass.h
@@ -0,0 +1,8 @@
+//
+// Created by zydiii on 22-7-9.
+//
+
+#ifndef ZENO_RENDERPASS_H
+#define ZENO_RENDERPASS_H
+
+#endif //ZENO_RENDERPASS_H
---
+++ zeno/zeno/extra/ResourceNode.h
@@ -0,0 +1,8 @@
+//
+// Created by zydiii on 22-7-9.
+//
+
+#ifndef ZENO_RESOURCEBASE_H
+#define ZENO_RESOURCEBASE_H
+
+#endif //ZENO_RESOURCEBASE_H

7b49301cc354e709ab23be54eb59ccee6a3bd492
zydiii <zydiiii@outlook.com>
2022-07-10 03:07:16 +0800

    [Modify Struct]RenderPass

--- zeno/zeno/extra/RenderPass.cpp
+++ zeno/zeno/extra/RenderPass.cpp
@@ -1,10 +1,26 @@
 #include <zeno/extra/RenderPass.h>
 
 namespace zeno {
-ZENO_API RenderPass::RenderPass(const std::string &name)
+ZENO_API RenderPassBase::RenderPassBase(const std::string &name)
     : name(name), seen(false), refCount(0) {}
 
-ZENO_API RenderPass::~RenderPass() = default;
+ZENO_API RenderPassBase::~RenderPassBase() = default;
 
+template<typename DataType>
+ZENO_API RenderPass<DataType>::RenderPass(const std::string &name, std::function<void(DataType &)> setupFunc, std::function<void(const DataType &)> renderFunc)
+    : RenderPassBase(name), setupFunc(setupFunc), renderFunc(renderFunc) {}
+
+template<typename DataType>
+ZENO_API RenderPass<DataType>::~RenderPass() = default;
+
+template<typename DataType>
+ZENO_API void RenderPass<DataType>::setup() {
+    setupFunc(data);
+}
+
+template<typename DataType>
+ZENO_API void RenderPass<DataType>::render() {
+    renderFunc(data);
+}
 
 }
--- zeno/zeno/extra/RenderPass.h
+++ zeno/zeno/extra/RenderPass.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include "ResourceNode.h"
+#include <zeno/extra/ResourceNode.h>
 #include <string>
 #include <vector>
-#include <zeno/extra/ResourceNode.h>
 
 namespace zeno {
     struct ResourceBase;
 
-    struct RenderPass {
+    struct RenderPassBase {
         std::string name;
         bool seen;
         std::vector<const ResourceBase*> creates;
@@ -16,10 +15,23 @@ namespace zeno {
         std::vector<const ResourceBase*> writes;
         std::size_t refCount;
 
-        ZENO_API RenderPass(const std::string& name);
-        ZENO_API ~RenderPass();
+        ZENO_API RenderPassBase(const std::string& name);
+        ZENO_API ~RenderPassBase();
 
         ZENO_API virtual void setup() = 0;
         ZENO_API virtual void render() = 0;
+    };
+
+    template<typename DataType>
+    struct RenderPass : RenderPassBase {
+        DataType data;
+
+        ZENO_API RenderPass(const std::string& name, std::function<void(DataType&)> setupFunc, std::function<void(const DataType&)> renderFunc);
+        ZENO_API ~RenderPass();
+
+        ZENO_API const std::function<void(DataType&)> setupFunc;
+        ZENO_API const std::function<void(const DataType&)> renderFunc;
+        ZENO_API virtual void setup() override;
+        ZENO_API virtual void render() override;
     };
 }
